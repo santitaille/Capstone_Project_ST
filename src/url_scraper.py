eefbd9313a553bb18e571832cedc24bfddf47b48
Santiago Tailleferd <santiago.tailleferd@unil.ch>
2025-11-20 18:50:25 +0000

    Added error handling + timeout for network requests

--- src/url_scraper.py
+++ src/url_scraper.py
@@ -18,24 +18,38 @@ OUTPUT_FILE = "/files/Capstone_Project_ST/data/player_urls.csv"
 def scrape_player_urls(base_url: str, total_pages: int):
     """Scrape player URLs from Futbin website."""
     all_urls = []
-    
+    failed_pages = []
+
     for page in range(1, total_pages + 1):
         url = base_url.format(page)
         print(f"Scraping page {page}/{total_pages}")
         
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        
-        rows = soup.select("table.futbin-table.players-table tbody tr") # Find player URLs from each table row
-        for row in rows: 
-            a_tag = row.select_one("td.table-name a")
-            if a_tag and a_tag.get("href"):
-                full_url = "https://www.futbin.com" + a_tag.get("href")
-                all_urls.append(full_url)
+        try:
+            response = requests.get(url, timeout=10) # So the program does not run indefinitely in case of connection problems
+            response.raise_for_status()
+            soup = BeautifulSoup(response.text, "html.parser")
+            
+            # Find player URLs from each table row
+            rows = soup.select("table.futbin-table.players-table tbody tr")
+            for row in rows:
+                a_tag = row.select_one("td.table-name a")
+                if a_tag and a_tag.get("href"):
+                    full_url = "https://www.futbin.com" + a_tag.get("href")
+                    all_urls.append(full_url)
+
+        # Ackowledge if there was an error
+        except requests.exceptions.RequestException as e:
+            print(f"Error on page {page}: {e}")
+            failed_pages.append(page)
+            continue
+    
+    if failed_pages:
+        print(f"{failed_pages} pages failed to scrape.")
     
     return all_urls
 
-if __name__ == "__main__": # Scrape and save URLs to CSV
+# Scrape and save URLs to CSV file
+if __name__ == "__main__":
     urls = scrape_player_urls(BASE_URL, TOTAL_PAGES)
     df = pd.DataFrame({"url": urls})
     df.to_csv(OUTPUT_FILE, index=False)
