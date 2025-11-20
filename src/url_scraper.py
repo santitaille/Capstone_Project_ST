c770f2b459d222403a338798b56e540e135f9133
Santiago Tailleferd <santiago.tailleferd@unil.ch>
2025-11-20 19:07:21 +0000

    Added logging + delay time between requests

--- src/url_scraper.py
+++ src/url_scraper.py
@@ -2,9 +2,16 @@
 URL scraper for EA FC 26 Players from Futbin website.
 """
 
+import time
+import logging
+
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+
+# Setup logging
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+logger = logging.getLogger(__name__)
 
 # Manually setted up Futbin website to find the URL for list of all 83+ OVR non-goalkeepers tradeable rated players
 BASE_URL = (
@@ -12,7 +19,7 @@ BASE_URL = (
     "position=LB%2CCB%2CRB%2CCAM%2CCM%2CCDM%2CRM%2CLM%2CST%2CRW%2CLW&"
     "pos_type=all&player_rating=83-99&eUnt=1"
 )
-TOTAL_PAGES = 2 # Checked first for small amount of pages
+TOTAL_PAGES = 2 # Tested first for small amount of pages
 OUTPUT_FILE = "/files/Capstone_Project_ST/data/player_urls.csv"
 
 def scrape_player_urls(base_url: str, total_pages: int):
@@ -22,7 +29,7 @@ def scrape_player_urls(base_url: str, total_pages: int):
 
     for page in range(1, total_pages + 1):
         url = base_url.format(page)
-        print(f"Scraping page {page}/{total_pages}")
+        logger.info(f"Scraping page {page}/{total_pages}")
         
         try:
             response = requests.get(url, timeout=10) # So the program does not run indefinitely in case of connection problems
@@ -37,20 +44,25 @@ def scrape_player_urls(base_url: str, total_pages: int):
                     full_url = "https://www.futbin.com" + a_tag.get("href")
                     all_urls.append(full_url)
 
-        # Ackowledge if there was an error
+            time.sleep(1) # Avoid too many requests in a short time
+
+        # Ackowledge if there was an error on a specific page
         except requests.exceptions.RequestException as e:
-            print(f"Error on page {page}: {e}")
+            logger.error(f"Error on page {page}: {e}")
             failed_pages.append(page)
             continue
-    
+
+    # Ackowledge the number of pages that failed to scrape
     if failed_pages:
-        print(f"{failed_pages} pages failed to scrape.")
+        logger.warning(f"{failed_pages} pages failed to scrape.")
     
     return all_urls
 
 # Scrape and save URLs to CSV file
 if __name__ == "__main__":
+    logger.info("Starting URL scraper")
     urls = scrape_player_urls(BASE_URL, TOTAL_PAGES)
     df = pd.DataFrame({"url": urls})
     df.to_csv(OUTPUT_FILE, index=False)
-    print(f"Scraped {len(urls)} URLs")
+    logger.info(f"Scraped {len(urls)} URLs to {OUTPUT_FILE}")
+    
